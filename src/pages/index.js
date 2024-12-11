b1b1aabe5b2b51d9f35b4b6f0fbd6788d5374ba6
Andy <andy.weir@feathr.co>
2024-12-11 16:28:20 -0500

    changed link text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Engage, retain, and grow your support base</h1>
+    <p>Growing your nonprofit’s community requires more than traditional outreach methods. Automating your engagement efforts and creating personalized touchpoints — all in one platform — empowers you to grow your mission, retain supporters, and amplify your cause, without the complexity.</p>
+    <Link to="/page-2/">Get a demo</Link>
   </div>
 )
 
